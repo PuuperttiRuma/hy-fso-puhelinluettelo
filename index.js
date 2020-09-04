43184c30df45d9b46b0d42e89786916bd9c7f3c7
Tommi Haapanen <thaapane@student.jyu.fi>
2020-09-04 15:09:26 +0300

    3.5,3.6 Uuden kontaktin lisäys ja virheiden käsittely

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { req, res } = require("express");
 const app = express();
 
+app.use(express.json())
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -25,6 +27,10 @@ let persons = [
   },
 ];
 
+const generateId = () => {
+  return Math.floor((Math.random() * 1000000))
+}
+
 app.get("/", (req, res) => {
   res.send("<h1>Welcome!<h1>");
 });
@@ -45,6 +51,27 @@ app.get("/api/persons/:id", (req, res) => {
   person
     ? res.json(person)
     : res.status(404).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name){
+    return res.status(400).json({error: 'name missing'})
+  }
+  if (!body.number){
+    return res.status(400).json({error: 'phone number missing'})
+  }
+  if(persons.some(p => p.name === body.name)){
+    return res.status(400).json({error: 'name must be unique'})
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+  persons = persons.concat(person)
+  res.json(person)
 })
 
 app.delete('/api/persons/:id', (req, res) => {
