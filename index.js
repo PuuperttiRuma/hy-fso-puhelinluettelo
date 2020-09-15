39735ffaf8678b70bdc3f4cb928e59e27b5fda55
Tommi Haapanen <thaapane@student.jyu.fi>
2020-09-15 17:01:44 +0300

    3.7 morgan logger

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const { req, res } = require("express");
+const express = require('express')
+const morgan = require('morgan')
+const { request, response } = require('express')
 const app = express();
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -31,54 +33,60 @@ const generateId = () => {
   return Math.floor((Math.random() * 1000000))
 }
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome!<h1>");
+//#region Endpoints
+
+app.get("/", (request, response) => {
+  response.send("<h1>Welcome!<h1>");
 });
 
-app.get("/info", (req, res) => {
-  res.send(`
+app.get("/info", (request, response) => {
+  response.send(`
     <p>Phonebook has info for ${persons.length} people.</p>
     <p>${Date()}</p>`)
 })
 
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
   const person = persons.find(p => p.id === id)
   person
-    ? res.json(person)
-    : res.status(404).end()
+    ? response.json(person)
+    : response.status(404).end()
 })
 
-app.post('/api/persons', (req, res) => {
-  const body = req.body
+app.post('/api/persons', (request, response) => {
+  //Error handling
+  const body = request.body
   if (!body.name){
-    return res.status(400).json({error: 'name missing'})
+    return response.status(400).json({error: 'name missing'})
   }
   if (!body.number){
-    return res.status(400).json({error: 'phone number missing'})
+    return response.status(400).json({error: 'phone number missing'})
   }
   if(persons.some(p => p.name === body.name)){
-    return res.status(400).json({error: 'name must be unique'})
+    return response.status(400).json({error: 'name must be unique'})
   }
 
+  //Implementation
   const person = {
     name: body.name,
     number: body.number,
     id: generateId()
   }
   persons = persons.concat(person)
-  res.json(person)
+  response.json(person)
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
   persons = persons.filter(p => p.id !== id)
-  res.status(204).end()
+  response.status(204).end()
 })
+
+//#endregion 
 
 const PORT = 3001;
 app.listen(PORT, () => {
